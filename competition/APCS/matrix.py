ab8389eb3d4f77c7469886a0590bb627ab9ca971
yiyu0x <tako0903@icloud.com>
2018-04-17 20:47:06 +0800

    matrix

---
+++ competition/APCS/matrix.py
@@ -0,0 +1,23 @@
+
+
+R,C,M = map(int,(input().split()))
+
+matrix = []
+for i in range(R):
+	matrix.append(list(map(int,(input().split()))))
+
+mod = list(map(int,(input().split())))
+
+for i in mod:
+	# 0 is rotate
+	if i == 0:
+		matrix[:] = map(list,zip(*matrix[::-1]))
+	elif i == 1:
+	# 1 is flip
+		matrix[:] = matrix[::-1]
+
+print(len(matrix),len(matrix[0]))
+for row in matrix:
+	for column in row:
+		print(column,end=" ")
+	print()
