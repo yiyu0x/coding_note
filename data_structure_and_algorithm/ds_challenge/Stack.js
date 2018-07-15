e2925496bdc6016e00d92aa8b08b749fc0b73acd
aFish <tako0903@icloud.com>
2018-07-16 00:32:48 +0800

    stack.js

--- data_structure_and_algorithm/ds_challenge/Stack.js
+++ data_structure_and_algorithm/ds_challenge/Stack.js
@@ -1,27 +1,73 @@
 function Stack() {
 
-	let Stack = [];
+	let head = null;
+	let length = 0;
+
+	const Node = function (value) {
+		this.value = value;
+		this.next;
+	}
+
+	this.isEmpty = function () {
+		if ( head==null ) {
+			console.log('Queue is empty.')
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	this.push = function (value) {
-		Stack.push(value);
+		const node = new Node(value);
+		let current;
+
+		if ( head==null ) {
+			head = node;
+			length = 1;
+		} else {
+			current = head;
+			while ( current.next ) {
+				current = current.next;
+			}
+			current.next = node;
+			length++;
+		}
 	}
 
 	this.pop = function () {
-		if ( Stack.length==0 ) {
-			console.log('Stack is Empty. Can\'t pop.');
-			return;
+		
+		if ( !this.isEmpty() ) {
+			let current = head;
+			let counter = 0;
+			while ( current ) {
+				counter++;
+				if ( counter == length-1) {
+					current.next = null;
+					length--;
+				}
+				current = current.next;
+			}
 		}
-		Stack.pop();
+
 	}
 
 	this.print = function () {
-		return Stack;
+		if ( !this.isEmpty() ) {
+			let current = head;
+			while ( current ) {
+				console.log( current.value );
+				current = current.next;
+			}
+			console.log('length is : ',length);
+		}
 	}
 }
 
 var s = new Stack();
 s.push(1)
 s.push(2)
+s.push(3)
+s.pop()
 s.pop()
 s.push(3)
-console.log(s.print())
+s.print()
