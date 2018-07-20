fcaff538f4b5ab110154fe9995ce47b883886486
aFish <tako0903@icloud.com>
2018-07-20 11:56:10 +0800

    coloring

--- data_structure_and_algorithm/ds_challenge/RedBlackTree.js
+++ data_structure_and_algorithm/ds_challenge/RedBlackTree.js
@@ -1,3 +1,4 @@
+//ref:https://www.qiujiawei.com/redblacktree-2/
 const Node = function (value) {
 	this.value = value;
 	this.color = 'red';
@@ -30,7 +31,37 @@ function RBT () {
 		}
 
 	}
-
+	this.findType = function (current,father,uncle) {
+		if ( father.value <= uncle.value && current.value <= father.value ) return 1;
+		if ( father.value > uncle.value && current.value > father.value ) return 2;
+		if ( father.value <= uncle.value && current.value > father.value ) return 3;
+		if ( father.value > uncle.value && current.value <= father.value ) return 4;
+	}
+	this.rotate = function (current,father,uncle) {
+		switch ( this.findType(current,father,uncle) ) {
+			//LL
+			case 1:
+				this.LL();
+				console.log(1)
+				break;
+			//RR
+			case 2:
+				this.RR();
+				console.log(2)
+				break;
+			case 3:
+			//LR
+				this.LR();
+				console.log(3)
+				break;
+			case 4:
+			//RL
+				this.RL();
+				console.log(4)
+				break;
+		}
+			
+	}
 	this.recolor = function (current) {
 		// console.log(current.value)
 		let father = this.findParent(current.value);
@@ -42,15 +73,36 @@ function RBT () {
 			uncle = (grandfather.left==father) ? grandfather.right:grandfather.left;
 		}
 		// console.log(uncle.value)
-		if ( father ) {
+		if (grandfather==this.head ) {
 			father.color = 'black';
-			if ( uncle ) {
+			return;
+		}
+		//in this function , father's color already knew as red .
+		let flag = 1;
+		while ( father!=this.head) {
+			flag++;
+			// let father = this.findParent(current.value);
+			// let grandfather = this.findParent( this.findParent(current.value).value );
+			
+			if ( uncle && uncle.color == 'black' ) {
+				this.rotate(current,father,uncle);
+				return
+			}else if ( uncle && uncle.color == 'red') {
 				uncle.color = 'black';
 			}
-			if ( grandfather != this.head ) {
-				grandfather.color = 'red';
+			//coloring
+			if ( flag%2==0 ) {
+				father.color = 'black';
+			} else {
+				father.color = 'red';
+			}
+			//iterative
+			father = this.findParent( father.value );
+			if ( grandfather.left && grandfather.right ) {
+				uncle = (grandfather.left==father) ? grandfather.right:grandfather.left;
 			}
 		}
+		father.color = 'black';
 	}
 
 	this.insert = function (node,current=this.head) {
@@ -81,9 +133,14 @@ function RBT () {
 }
 
 var r = new RBT();
+r.insert(new Node(80));
+r.insert(new Node(40));
 r.insert(new Node(20));
-r.insert(new Node(14));
-r.insert(new Node(23));
-r.insert(new Node(13));
+r.insert(new Node(10));
+r.insert(new Node(9));
 // console.log(r.head);
 console.log(r.head);
+
+console.log();
+console.log()
+console.log(r.head.left.left);
