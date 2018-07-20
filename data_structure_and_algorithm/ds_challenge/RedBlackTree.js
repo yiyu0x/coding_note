f7a52f49f3b1bad7068a25879480e8bfb4d1b9d4
aFish <tako0903@icloud.com>
2018-07-20 10:34:29 +0800

    recolor

--- data_structure_and_algorithm/ds_challenge/RedBlackTree.js
+++ data_structure_and_algorithm/ds_challenge/RedBlackTree.js
@@ -11,16 +11,46 @@ function RBT () {
 	this.findParent = function (target,current=this.head) {
 
 		if ( current ) {
-			if ( current.left==target ) return current;
-			else if ( current.right==target ) return current;
-			return this.findParent(target, current.left);
-			return this.findParent(target, current.right);
+			if ( current.left ) {
+				if ( current.left.value == target ) return current;	
+				if ( this.findParent(target, current.left) ) { 
+					return this.findParent(target, current.left);
+				}
+				
+			}
+
+			if ( current.right ) {
+				if ( current.right.value == target ) return current;
+				if ( this.findParent(target, current.right) ) {
+					return this.findParent(target, current.right);
+				}
+			}
+
+			return false;
 		}
 
 	}
 
 	this.recolor = function (current) {
-		this.findParent(current);
+		// console.log(current.value)
+		let father = this.findParent(current.value);
+		// console.log(father.value)
+		let grandfather = this.findParent( this.findParent(current.value).value );
+		// console.log(grandfather.value)
+		let uncle = null;
+		if ( grandfather.left && grandfather.right ) {
+			uncle = (grandfather.left==father) ? grandfather.right:grandfather.left;
+		}
+		// console.log(uncle.value)
+		if ( father ) {
+			father.color = 'black';
+			if ( uncle ) {
+				uncle.color = 'black';
+			}
+			if ( grandfather != this.head ) {
+				grandfather.color = 'red';
+			}
+		}
 	}
 
 	this.insert = function (node,current=this.head) {
@@ -34,14 +64,16 @@ function RBT () {
 		if ( current ) {
 			if ( !current.left && node.value <= current.value ) {
 				current.left = node;
+				if ( current.color == 'red' ) this.recolor(node);
 				return;
 			} else if ( !current.right && node.value > current.value ) {
 				current.right = node;
+				if ( current.color == 'red' ) this.recolor(node);
 				return;
-			}
+			} 
+			if ( node.value <= current.value) this.insert( node, current.left );
+			else this.insert( node, current.right );
 
-			this.insert( node, current.left );
-			this.insert( node, current.right );
 		}
 
 
@@ -50,8 +82,8 @@ function RBT () {
 
 var r = new RBT();
 r.insert(new Node(20));
-r.insert(new Node(22));
-r.insert(new Node(26));
-r.insert(new Node(15));
+r.insert(new Node(14));
+r.insert(new Node(23));
+r.insert(new Node(13));
 // console.log(r.head);
-conosle.log(r.findParent(26));
+console.log(r.head);
